7bd30643ce5ea70acb2f4253c0c94bd06b6aae28
Roudan Haj Hussein <roudan.dev@gmail.com>
2019-06-20 23:18:48 +0300

    apply a11y standards

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -84,6 +84,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   name.innerHTML = restaurant.name;
 
   const address = document.getElementById('restaurant-address');
+  address.setAttribute("role", "adress");
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
